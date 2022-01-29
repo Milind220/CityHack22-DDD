6988545064c382577347a25a0ab9016874c80b44
Matthew Betty <matt.betty.1@gmail.com>
2022-01-30 00:20:12 +0800

    recommender system
    recommender system

--- lib/backend/recommendation_profile.dart
+++ lib/backend/recommendation_profile.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/gestures.dart';
 import 'package:sampleapp/backend/healthcare_provider.dart';
 import 'package:sampleapp/backend/recommendation_result.dart';
@@ -11,7 +13,7 @@ enum HealthcareProviderType {
 enum ConsultationType {
   inPerson,
   virtual,
-  noPrereference,
+  noPreference,
 }
 
 enum LocationType {
@@ -61,6 +63,117 @@ enum Value {
   childsTherapist,
 }
 
+String provTypeToString(HealthcareProviderType healhcareprovidertype) {
+  switch (healhcareprovidertype) {
+    case HealthcareProviderType.psychologist:
+      return 'In Person';
+    case HealthcareProviderType.psychiatrist:
+      return 'Virtual';
+    case HealthcareProviderType.supportGroup:
+      return 'Support Group';
+  }
+}
+
+String typeToString(ConsultationType consultationtype) {
+  switch (consultationtype) {
+    case ConsultationType.inPerson:
+      return 'In Person';
+    case ConsultationType.virtual:
+      return 'Virtual';
+    case ConsultationType.noPreference:
+      return 'noPreference';
+  }
+}
+
+String locationToString(LocationType locationtype) {
+  switch (locationtype) {
+    case LocationType.island:
+      return 'Hong Kong Island';
+    case LocationType.kowloon:
+      return 'Kowloon';
+    case LocationType.newTerritories:
+      return 'New Territories';
+    case LocationType.noPreference:
+      return 'noPreference';
+  }
+}
+
+String genderToString(Gender gender) {
+  switch (gender) {
+    case Gender.male:
+      return 'Male';
+    case Gender.female:
+      return 'Female';
+    case Gender.other:
+      return 'Other';
+  }
+}
+
+String ageToString(AgeGroup agegroup) {
+  switch (agegroup) {
+    case AgeGroup.young:
+      return 'Young';
+    case AgeGroup.middle:
+      return 'Middle Aged';
+    case AgeGroup.old:
+      return 'Old';
+  }
+}
+
+String treatToString(Treatments treatment) {
+  switch (treatment) {
+    case Treatments.cbt:
+      return 'Cognitive Behavioural Therapy';
+    case Treatments.pdt:
+      return 'Psychodynamic therapy';
+    case Treatments.ht:
+      return 'Humanistic therapy';
+    case Treatments.pat:
+      return 'Psychoanalytic therapy';
+    case Treatments.it:
+      return 'Integrative or holistic therapy';
+    case Treatments.couples:
+      return 'Couples therapy';
+    case Treatments.notSure:
+      return 'notSure';
+  }
+}
+
+String valToString(Value value) {
+  switch (value) {
+    case Value.lgbtq:
+      return 'LGBTQ';
+    case Value.religion:
+      return 'Religion';
+    case Value.youthServices:
+      return 'Youth Services';
+    case Value.addiction:
+      return 'Addiction';
+    case Value.abuse:
+      return 'Abuse';
+    case Value.mentalDisorder:
+      return 'Mental Disorder';
+    case Value.alternativeTreatments:
+      return 'Alternative Treatments';
+    case Value.availability:
+      return 'Availability';
+    case Value.depression:
+      return 'Depression';
+    case Value.chronicAnxiety:
+      return 'Chronic Anxiety';
+    case Value.adhd:
+      return 'ADHD';
+    case Value.add:
+      return 'ADD';
+    case Value.educationalPsychology:
+      return 'Educational Psychology';
+    case Value.racialTrauma:
+      return 'Racial Trauma';
+    case Value.childsTherapist:
+      return 'Child Therapist';
+  }
+}
+
 class RecommendationProfile {
   final HealthcareProviderType _providerType;
   final ConsultationType _consultationType;
@@ -89,7 +202,80 @@ class RecommendationProfile {
 
   RecommendationResult compare(
       RecommendationProfile other, HealthcareProvider provider) {
-    double similarity = 1;
-    return RecommendationResult(similarity, [], [], provider);
+    List<String> matches = [];
+    List<String> mismatches = [];
+    double similarity = 0;
+
+    if (_providerType == other.providerType) {
+      similarity++;
+      matches.add(provTypeToString(_providerType));
+    } else {
+      similarity--;
+      mismatches.add(provTypeToString(_providerType));
+    }
+
+    if (_consultationType != ConsultationType.noPreference) {
+      if (_consultationType == other.consultationType) {
+        similarity++;
+        matches.add(typeToString(_consultationType));
+      } else {
+        similarity--;
+        mismatches.add(typeToString(_consultationType));
+      }
+    }
+
+    if (_location != LocationType.noPreference) {
+      if (_location == other.location) {
+        similarity++;
+        matches.add(locationToString(_location));
+      } else {
+        similarity--;
+        mismatches.add(locationToString(_location));
+      }
+    }
+
+    if (_gender == other.gender) {
+      similarity++;
+      matches.add(genderToString(_gender));
+    } else {
+      similarity--;
+      mismatches.add(genderToString(gender));
+    }
+
+    if (_ageGroup == other.ageGroup) {
+      similarity++;
+      matches.add(ageToString(_ageGroup));
+    } else {
+      similarity--;
+      mismatches.add(ageToString(_ageGroup));
+    }
+
+    if (_treatments != Treatments.notSure) {
+      if (_treatments == other.treatments) {
+        similarity++;
+        matches.add(treatToString(_treatments));
+      } else {
+        similarity--;
+        mismatches.add(treatToString(_treatments));
+      }
+    }
+
+    for (var i = 0; i < _values.length; i++) {
+      int idx = -1;
+      for (var j = 0; j < other.values.length; j++) {
+        if (_values[i] == other.values[j]) {
+          idx = i;
+          break;
+        }
+      }
+      if (idx != -1) {
+        matches.add(valToString(_values[i]));
+        similarity++;
+      } else {
+        mismatches.add(valToString(_values[i]));
+        similarity--;
+      }
+    }
+    return RecommendationResult(similarity, matches, mismatches, provider);
   }
 }
