f3f6ae2ddf50288fef610745be6ff1873e1e1edb
AlexScriba <61542302+AlexScriba@users.noreply.github.com>
2022-01-30 04:49:02 +0800

    Temp commit

--- lib/pages/preferences.dart
+++ lib/pages/preferences.dart
@@ -1,8 +1,11 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:sampleapp/components/drop_down.dart';
+import 'package:sampleapp/components/page_view_tab.dart';
 import 'package:sampleapp/components/crisisbutton.dart';
 import 'package:sampleapp/components/normalbutton.dart';
+import 'package:sampleapp/components/prompt_text.dart';
 import 'package:sampleapp/utils/consts.dart';
 
 class PreferenceScreen extends StatelessWidget {
@@ -12,11 +15,14 @@ class PreferenceScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-          child: Column(
-        children: [
-          FirstTab(),
-        ],
-      )),
+        child: ListView(
+          children: [
+            FirstTab(),
+            ProviderTypeTab(),
+            FurtherQuestionsTab(),
+          ],
+        ),
+      ),
       floatingActionButton: CrisisButton(
         onClicFunc: () {
           Navigator.pushNamed(context, '/crisis');
@@ -53,5 +59,138 @@ class FirstTab extends StatelessWidget {
         ),
       ),
     );
+  }
+}
+
+class ProviderTypeTab extends StatelessWidget {
+  const ProviderTypeTab({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return PageViewTab(
+      children: [
+        PromptText('Are you ready to input your preferences now?'),
+        NormalButton(buttonText: 'Psychiatrist', onClickFunc: () => {}),
+        NormalButton(buttonText: 'Psychologist', onClickFunc: () => {}),
+        NormalButton(buttonText: 'Support Group', onClickFunc: () => {}),
+        NormalButton(buttonText: 'Not Sure', onClickFunc: () => {}),
+      ],
+    );
+  }
+}
+
+class FurtherQuestionsTab extends StatelessWidget {
+  const FurtherQuestionsTab({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return PageViewTab(
+      children: [
+        PromptText('How would prefer sessions to be delivered?'),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Expanded(
+              flex: 1,
+              child: NormalButton(
+                buttonText: 'Virtual',
+                onClickFunc: () => {},
+              ),
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            Expanded(
+              flex: 1,
+              child: NormalButton(
+                buttonText: 'Virtual',
+                onClickFunc: () => {},
+              ),
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            Expanded(
+              flex: 1,
+              child: NormalButton(
+                buttonText: 'Virtual',
+                onClickFunc: () => {},
+              ),
+            ),
+          ],
+        ),
+        PromptText('What is your preferred cost/pricing bracket?'),
+        DropDownList(
+          options: <String>['English', 'Cantonese', 'Mandarin'],
+        ),
+        PromptText('What is your preferred cost/pricing bracket?'),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Expanded(
+              flex: 1,
+              child: NormalButton(
+                buttonText: 'Virtual',
+                onClickFunc: () => {},
+              ),
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            Expanded(
+              flex: 1,
+              child: NormalButton(
+                buttonText: 'Virtual',
+                onClickFunc: () => {},
+              ),
+            ),
+          ],
+        ),
+        PromptText('Age group?'),
+        DropDownList(
+          options: ['20 - 30', '30 - 50', '50+'],
+        ),
+        PromptText('Treatment type:'),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Expanded(
+              flex: 1,
+              child: NormalButton(
+                buttonText: 'Virtual',
+                onClickFunc: () => {},
+              ),
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            Expanded(
+              flex: 1,
+              child: NormalButton(
+                buttonText: 'Virtual',
+                onClickFunc: () => {},
+              ),
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            Expanded(
+              flex: 1,
+              child: NormalButton(
+                buttonText: 'Virtual',
+                onClickFunc: () => {},
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class ValuesTab extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
