1d7c3a50efd83ccef0ee819f51a0401460b4e3fb
ChamberlinMM <82706361+ChamberlinMM@users.noreply.github.com>
2022-01-30 04:48:32 +0800

    Almost finished feed

--- lib/components/doctorlisttile.dart
+++ lib/components/doctorlisttile.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:sampleapp/utils/consts.dart';
+import '../components/pillcomponent.dart';
 
 class DoctorListTile extends StatelessWidget {
   const DoctorListTile({Key? key}) : super(key: key);
@@ -7,9 +9,21 @@ class DoctorListTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
       child: Container(
+        margin: EdgeInsets.only(bottom: 15),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: Colors.white,
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black45,
+                spreadRadius: 0.5,
+                blurRadius: 7.5,
+                offset: Offset(2, 3),
+              ),
+            ]),
         //alignment: Alignment.center,
-        width: 300,
-        color: Colors.red,
+        width: 302,
+        //color: c_primaryColor,
         child: Column(
           //mainAxisAlignment: MainAxisAlignment.center,
           //crossAxisAlignment: CrossAxisAlignment.center,
@@ -29,14 +43,35 @@ class NameArea extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      decoration: BoxDecoration(borderRadius: BorderRadius.circular(10)),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          Text('Doc image'),
+          Container(
+            width: 100,
+            child: const Icon(Icons.account_box_sharp),
+          ),
           Column(
             children: [
-              Text('Doc Name'),
-              Text('Doc Blurb'),
+              Container(
+                padding: EdgeInsets.all(10),
+                child: Text(
+                  'Doc Name',
+                  style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(10),
+                height: 90,
+                width: 200,
+                child: Text(
+                  'Doc Blurb kwafnio ofihwqe qf 9uw 9qu we-0fw90 w- w-0qiwfou09wu 09w9u wf-uwfq 09wfq-0',
+                  style: TextStyle(
+                    fontSize: 14,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
             ],
           ),
         ],
@@ -56,18 +91,21 @@ class PillArea extends StatelessWidget {
         children: [
           Column(
             children: [
-              Text('first match'),
-              Text('Second Match'),
-              Text('Third Match')
+              PillComponent(PillColor: Colors.greenAccent, PillText: 'Match'),
+              PillComponent(PillColor: Colors.greenAccent, PillText: 'Match'),
+              PillComponent(PillColor: Colors.greenAccent, PillText: 'Match'),
             ],
           ),
           Column(
             children: [
               Column(
                 children: [
-                  Text('First Mismatch'),
-                  Text('Second Mismatch'),
-                  Text('Third Mismatch')
+                  PillComponent(
+                      PillColor: Colors.redAccent, PillText: 'misMatch'),
+                  PillComponent(
+                      PillColor: Colors.redAccent, PillText: 'misMatch'),
+                  PillComponent(
+                      PillColor: Colors.redAccent, PillText: 'misMatch'),
                 ],
               ),
             ],
--- lib/pages/main_feed.dart
+++ lib/pages/main_feed.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sampleapp/utils/consts.dart';
 import '../components/crisisbutton.dart';
 import '../components/doctorlisttile.dart';
 
@@ -8,16 +9,103 @@ class MainFeedScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        alignment: Alignment.center,
-        child: Column(
-          //mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              child: DoctorListTile(),
+      body: ListView(
+        children: [
+          Container(
+            alignment: Alignment.center,
+            child: Column(
+              //mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(height: 30),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Container(
+                      width: 150,
+                      margin: EdgeInsets.only(bottom: 10),
+                      alignment: Alignment.center,
+                      padding: EdgeInsets.all(5),
+                      child: Text('Resource Center',
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          textAlign: TextAlign.center),
+                      decoration: BoxDecoration(
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black45,
+                              spreadRadius: 0.10,
+                              blurRadius: 2,
+                              offset: Offset(2, 3),
+                            ),
+                          ],
+                          borderRadius: BorderRadius.circular(10),
+                          color: c_primaryColor),
+                    ),
+                    Container(
+                      width: 150,
+                      margin: EdgeInsets.only(bottom: 10),
+                      alignment: Alignment.center,
+                      padding: EdgeInsets.all(5),
+                      child: Text('Progress Tracker',
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                          textAlign: TextAlign.center),
+                      decoration: BoxDecoration(
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black45,
+                              spreadRadius: 0.10,
+                              blurRadius: 2,
+                              offset: Offset(2, 3),
+                            ),
+                          ],
+                          borderRadius: BorderRadius.circular(10),
+                          color: c_primaryColor),
+                    ),
+                  ],
+                ),
+                Container(
+                  child: Column(
+                    children: [
+                      GestureDetector(
+                        child: DoctorListTile(),
+                        onTap: () => {
+                          Navigator.pushNamed(context, '/docprofile'),
+                        },
+                      ),
+                      GestureDetector(
+                        child: DoctorListTile(),
+                        onTap: () => {
+                          Navigator.pushNamed(context, '/docprofile'),
+                        },
+                      ),
+                      GestureDetector(
+                        child: DoctorListTile(),
+                        onTap: () => {
+                          Navigator.pushNamed(context, '/docprofile'),
+                        },
+                      ),
+                      GestureDetector(
+                        child: DoctorListTile(),
+                        onTap: () => {
+                          Navigator.pushNamed(context, '/docprofile'),
+                        },
+                      ),
+                      GestureDetector(
+                        child: DoctorListTile(),
+                        onTap: () => {
+                          Navigator.pushNamed(context, '/docprofile'),
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
