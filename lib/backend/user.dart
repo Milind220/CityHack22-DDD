b09e79a67f702eff59675d0abdcbfec6baf84462
Milind Sharma <milindsharma8@gmail.com>
2022-01-29 17:12:51 +0530

    Add recommendation profile to user class

--- lib/backend/user.dart
+++ lib/backend/user.dart
@@ -1,9 +1,11 @@
 class User {
   final String? name;
   final String? userId;
+  final RecommendationProfile? profile;
 
   User({
     this.name,
     this.userId,
+    this.RecommendationProfile,
   });
 }
