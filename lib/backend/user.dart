96777f0df1c968aaa1fd99730b62d4f0d495591b
Milind Sharma <milindsharma8@gmail.com>
2022-01-29 18:09:52 +0530

    Remove final declaration for profile in User class

--- lib/backend/user.dart
+++ lib/backend/user.dart
@@ -1,11 +1,9 @@
-import 'package:sampleapp/backend/booking.dart';
 import 'package:sampleapp/backend/recommendation_profile.dart';
 
 class User {
-  // Made them potentially nullable for the anonymous user feature
   final String _name;
   final String _userId;
-  final RecommendationProfile? profile;
+  RecommendationProfile? profile;
 
   User(
     this._name,
