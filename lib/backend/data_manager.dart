7dc3e42b977b4cade57329d102ad2f700e692f88
Milind Sharma <milindsharma8@gmail.com>
2022-01-30 02:58:30 +0530

    Add things

--- lib/backend/data_manager.dart
+++ lib/backend/data_manager.dart
@@ -1,7 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'dart:async';
 import 'package:rxdart/rxdart.dart';
+import 'package:sampleapp/backend/healthcare_provider.dart';
 import 'package:sampleapp/backend/login_manager.dart';
+import 'package:sampleapp/backend/user.dart';
 
 class DataManager {
   final FirebaseFirestore _db = FirebaseFirestore.instance;
@@ -51,4 +53,27 @@ class DataManager {
 
     return ref.set(data, SetOptions(merge: true));
   }
+
+  // Read user data from fires
+  // Future<List<HealthcareProvider>> getAllDoctors() async {
+  //   var ref = _db.collection('providers');
+  //   var snapshot = await ref.get();
+  //   var data = snapshot.docs.map((s) => s.data()).toList();
+
+  //   for (var provider_data in data) {
+  //     String name = provider_data['name'];
+  //     String address = provider_data['address'];
+  //     Map<String, String> questions = provider_data['questions'];
+  //     List<int> profile = provider_data['recommendation_profile'];
+  //   }
+  //   ;
+  // }
+
+  /// Retrieves a single quiz document
+  // Future<User> getUserData() async {
+  //   var user = LoginManager.instance.user!;
+  //   var ref = _db.collection('users').doc(user.uid);
+  //   var snapshot = await ref.get();
+  //   return Quiz.fromJson(snapshot.data() ?? {});
+  // }
 }
