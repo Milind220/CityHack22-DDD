1fc1b096d7702adc074e9d521cad73dca44a7271
Milind Sharma <milindsharma8@gmail.com>
2022-01-30 01:09:39 +0530

    Fix addUser method

--- lib/backend/data_manager.dart
+++ lib/backend/data_manager.dart
@@ -9,32 +9,44 @@ class DataManager {
   Future<void> addUser(
     String name,
     bool hasProfile,
-    List<int>? recommendationProfile,
+    List<dynamic> recommendationProfile,
   ) async {
     var user = LoginManager.instance.user!;
     CollectionReference users = _db.collection('users');
-
-    await _db
-        .collection('users')
-        .doc(user.uid)
-        .set({
-          'name': name,
-          'has_profile': hasProfile,
-          'recommendation_profile': recommendationProfile,
-        })
-        .then((value) => print("User Added"))
-        .catchError((error) => print("Failed to add user: $error"));
+    if (hasProfile) {
+      await _db
+          .collection('users')
+          .doc(user.uid)
+          .set({
+            'name': name,
+            'has_profile': hasProfile,
+            'recommendation_profile':
+                FieldValue.arrayUnion(recommendationProfile),
+          })
+          .then((value) => print("User Added"))
+          .catchError((error) => print("Failed to add user: $error"));
+    } else {
+      await _db
+          .collection('users')
+          .doc(user.uid)
+          .set({
+            'name': name,
+            'has_profile': hasProfile, // Doesn't set recommendation profile
+          })
+          .then((value) => print("User Added"))
+          .catchError((error) => print("Failed to add user: $error"));
+    }
   }
 
   Future<void> updateUserProfile(
-    List<int>? recommendationProfile,
+    List<dynamic> recommendationProfile,
   ) async {
     var user = LoginManager.instance.user!;
     var ref = _db.collection('users').doc(user.uid);
 
     var data = {
       'has_profile': true,
-      'recommendation_profile': recommendationProfile
+      'recommendation_profile': FieldValue.arrayUnion(recommendationProfile),
     };
 
     return ref.set(data, SetOptions(merge: true));
