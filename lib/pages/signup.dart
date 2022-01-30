5e3e10e90139f21fd848bd55e839519cbc0db072
Milind Sharma <milindsharma8@gmail.com>
2022-01-30 09:48:13 +0530

    Milind changes

--- lib/pages/signup.dart
+++ lib/pages/signup.dart
@@ -12,41 +12,43 @@ class SignupScreen extends StatelessWidget {
     return Scaffold(
       body: Container(
           padding: const EdgeInsets.all(45.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Image.asset('assets/images/png_logo.png'),
-              TextField(
-                decoration: InputDecoration(hintText: 'Name'),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              TextField(
-                decoration: InputDecoration(hintText: 'Email'),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              TextField(
-                decoration: InputDecoration(hintText: 'Number'),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              TextField(
-                decoration: InputDecoration(hintText: 'Password'),
-              ),
-              Container(
-                  margin: const EdgeInsets.only(top: 75.0),
-                  child: NormalButton(
-                      buttonText: 'Signup',
-                      onClickFunc: () {
-                        loggedInUser = DataManager.instance.getUserData();
-                        Navigator.pushNamed(context, '/prefs');
-                      }))
-            ],
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Image.asset('assets/images/png_logo.png'),
+                TextField(
+                  decoration: InputDecoration(hintText: 'Name'),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                TextField(
+                  decoration: InputDecoration(hintText: 'Email'),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                TextField(
+                  decoration: InputDecoration(hintText: 'Number'),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                TextField(
+                  decoration: InputDecoration(hintText: 'Password'),
+                ),
+                Container(
+                    margin: const EdgeInsets.only(top: 75.0),
+                    child: NormalButton(
+                        buttonText: 'Signup',
+                        onClickFunc: () {
+                          loggedInUser = DataManager.instance.getUserData();
+                          Navigator.pushNamed(context, '/prefs');
+                        }))
+              ],
+            ),
           )),
       floatingActionButton: CrisisButton(
         onClicFunc: () {
