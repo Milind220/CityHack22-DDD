858a196f3cccb43e292bb54c12f1b63b918060e3
AlexScriba <61542302+AlexScriba@users.noreply.github.com>
2022-01-30 08:14:28 +0800

    Added data class

---
+++ lib/utils/data.dart
@@ -0,0 +1,19 @@
+final List<Map<String, dynamic>> providers = [
+  {
+    'name': 'Alex Scriba',
+    'address': '7 Nathan Road, Yau Ma Tei',
+    'questions': [
+      {
+        'question': 'Are you open at nights?',
+        'answer': 'No we are only open during normal working hours.',
+      }
+    ],
+    'providerType': 0,
+    'consultationType': 1,
+    'location': 1,
+    'gender': 0,
+    'ageGroup': 1,
+    'treatments': 2,
+    'values': [0, 2, 1, 7]
+  }
+];
